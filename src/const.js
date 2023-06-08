13ff9093a5dc78b951572a829ebf4d604caed162
danilamdev <dalmabarri@gmail.com>
2023-06-07 23:31:16 -0300

    add navbar scroll effect

---
+++ src/const.js
@@ -0,0 +1,7 @@
+export const NAVLINKS = {
+  "/": { href: "/", title: "HOME" },
+  "/cursos": { href: "/cursos", title: "CURSOS" },
+  "/sobre-mi": { href: "/sobre-mi", title: "SOBRE MI" }
+} 
+
+export const HEAD_TITLE = 'Carlos Lambarri'
