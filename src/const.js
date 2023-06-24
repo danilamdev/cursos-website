0fa6e4dffadad22af9366ad720dc2d4f59a43e18
danilamdev <dalmabarri@gmail.com>
2023-06-24 12:20:48 -0300

    fix performance images

--- src/const.js
+++ src/const.js
@@ -4,4 +4,42 @@ export const NAVLINKS = {
   "/sobre-mi": { href: "/sobre-mi", title: "SOBRE MI" }
 } 
 
+export const stats = [
+  {
+    number: "280+",
+    title: "cursos",
+  },
+  {
+    number: "110K+",
+    title: "Proyecciones",
+  },
+  {
+    number: "18K+",
+    title: "Horas cátedra",
+  },
+];
+
+export const beneficios = [
+  {
+    title: "Capacitación",
+    content:
+      "Brindando capacitación en los últimos avances tecnológicos orientados al soporte de decisiones de máximo interés en las organizaciones.",
+  },
+  {
+    title: "Especialización",
+    content:
+      "Permitiendo a los alumnos su especialización en el Estado del Arte en aquellos temas de pinta en las organizaciones.",
+  },
+  {
+    title: "Actualización",
+    content:
+      "Ofreciendo actualización del estado en que se encuentran las tecnologías que conforman el vértice superior del conocimiento en el Management de  vanguardia.",
+  },
+  {
+    title: "Oportunidad",
+    content:
+      "Elevando el valor de tus estudiantes en el mercado laboral, el cual crecerá ante la alta demanda que existe en estas especializaciones.",
+  },
+];
+
 export const HEAD_TITLE = 'Carlos Lambarri'
